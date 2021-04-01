9760407edd37cf7eef1aab8f6830ac70a7906f0b
Rodolphe <dutchr@live.fr>
2021-04-01 16:53:03 +0200

    WIP
    adding context on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react'
 
 import { AddNewItem } from './components/AddNewItem';
+import { useAppState } from './components/AppStateContext';
 import { Card } from './components/Card';
 import { Column } from './components/Column';
 import { AppContainer } from './styles';
 
+const counterReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "increment":
+      return {count : state.count +1}
+    case "decrement":
+      return {count : state.count -1}
+    default:
+      throw new Error()
+  }
+}
+
+interface State {
+  count: number;
+}
+interface Action {
+  type: string;
+}
 
  const App = () => {
+   const { state } = useAppState()
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Faire un composant"/>
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript"/>
-      </Column>
-      <Column text="Done">
-        <Card text="Almost finish my tasks"/>
-      </Column>
-      <AddNewItem toggleButtonText="+ Ajouter une liste" onAdd={console.log} />
+      { state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i}/>
+      ))}
+      <AddNewItem
+      toggleButtonText="+ Ajouter une nouvelle liste"
+      onAdd={console.log}
+      />
     </AppContainer>
   )
 }
---
+++ src/components/AppStateContext.tsx
@@ -0,0 +1,98 @@
+import React, { createContext, useContext,useReducer } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import { findItemIndexById } from '../utils/findItemIndexById';
+const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps)
+
+export const AppStateProvider = ({children}: React.PropsWithChildren<{}>) => {
+  const [state, dispatch] = useReducer(appStateReducer, appData)
+  return(
+    <AppStateContext.Provider value={{state, dispatch}}>
+      {children}
+    </AppStateContext.Provider>
+  )
+}
+export const useAppState = () => {
+  return useContext(AppStateContext)
+}
+
+type Action = 
+| {
+  type: "ADD_LIST"
+  payload:string
+  }
+| {
+  type: "ADD_TASK"
+  payload:{ text: string; taskId:string}
+  }
+
+
+const appStateReducer = (state: AppState, action: Action): AppState => {
+  switch (action.type) {
+    case "ADD_LIST":{
+      const visibilityExemple = "Too visible"
+      return {
+        ...state,
+        lists: [
+          ...state.lists,
+          {id:uuidv4(), text: action.payload, tasks:[]}
+        ]
+      }
+    }
+    case "ADD_TASK": {
+      const visibilityExemple = "Too visible"
+      const targetLaneIndex = findItemIndexById(
+        state.lists,
+        action.payload.taskId
+      )
+      state.lists[targetLaneIndex].tasks.push({
+        id: uuidv4(),
+        text: action.payload.text
+      })
+      return {
+        ...state
+      }
+    }
+    default: { 
+      return state
+    }
+  }
+}
+
+const appData: AppState = {
+  lists: [
+    {
+      id:"0",
+      text:"To Do",
+      tasks:[{ id:"c0", text:"Create Component"}]
+    },
+    {
+      id:"0",
+      text:"In Progress",
+      tasks:[{ id:"c2", text:"Learning Typescript"}]
+    },
+    {
+      id:"0",
+      text:"Done",
+      tasks:[{ id:"c3", text:"Almost finish my tasks"}]
+    },
+  ],
+}
+
+interface AppStateContextProps {
+  state: AppState
+}
+interface Task {
+  id: string,
+  text: string
+}
+interface List {
+  id: string,
+  text: string,
+  tasks: Task[]
+}
+
+export interface AppState {
+  lists: List[]
+}
+
+
